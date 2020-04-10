faf33c493e593079d3141d5b79aaf29e1b183d55
Evaristo Cuesta <evaristocuesta@outlook.com>
2020-04-10 20:07:10 +0200

    Se ha cambiado el ToleranceToColorConverter a BoolToColorConverter. Además se añaden propiedades para poder elegir el color para true y false.

--- EspejoAnalysis/Converter/BoolToColorConverter.cs
+++ EspejoAnalysis/Converter/BoolToColorConverter.cs
@@ -5,16 +5,20 @@ using System.Windows.Media;
 
 namespace EspejoAnalysis.Converter
 {
-    public class ToleranceToColorConverter : IValueConverter
+    public class BoolToColorConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return ((bool)value) ? Brushes.LimeGreen : Brushes.Red;
+            return ((bool)value) ? ColorTrue : ColorFalse;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
         }
+
+        public Brush ColorTrue { get; set; }
+
+        public Brush ColorFalse { get; set; }
     }
 }
